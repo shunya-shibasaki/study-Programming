1d9d04c5a32b180175d6dd86931509cb6e5b00bc
shunya_shibasaki <shunya1120pj@gmail.com>
2023-06-12 22:38:24 +0900

    Vue新規コミット

---
+++ vuesample/src/components/SampleComponents.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-header class="pa-0">
+      <h3>サンプルコンポーネント</h3>
+    </v-expansion-panel-header>
+  <v-expansion-panel-content>
+    
+  </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+
+<script>
+import Vue from "vue";
+import FilterSample from "@/components/FilterSample.vue"
+import SampleCalendar from "@/components/SampleCalendar.vue"
+
+export default Vue.extend({
+  components: {
+    FilterSample,
+    SampleCalendar,
+  }
+})
+</script>

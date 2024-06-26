d94a1c08bd195a89a6f4e13e5b52304f8ccea18e
shunya_shibasaki <shunya1120pj@gmail.com>
2024-06-27 00:04:25 +0900

    nuxt3のレクチャー

---
+++ nuxt3-acync/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img src="~/assets/カワイクテゴメンネ.jpg" alt="touhou">
+    <!-- <p> {{ users[0].id }}, {{ users[0].name }}</p> -->
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }}, {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const { data: users, error } = await useFetch('https://jsonplaceholder.typicode.com/users')
+  // console.log(error.value)
+ if (error.value) {
+   throw createError({ statusCode: 404, statusMessage: 'page not found!!!!'})
+   }
+</script>
